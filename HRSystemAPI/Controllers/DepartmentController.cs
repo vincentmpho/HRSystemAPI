76696dd08c4343aaf761197644ae581d78c0435a
Tshindimpho <mudzu13@gmail.com>
2024-01-19 00:18:36 +0200

    Implement Endpoint for Department and Persons

---
+++ HRSystemAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,101 @@
+﻿using HRSystemAPI.Data;
+using HRSystemAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly HRDbContext _hRDbContext;
+
+        public DepartmentController(HRDbContext hRDbContext)
+        {
+            _hRDbContext = hRDbContext;
+        }
+
+        // GET: api/Department
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartments()
+        {
+            return StatusCode(StatusCodes.Status200OK, await _hRDbContext.Departments.ToListAsync());
+        }
+
+        // GET: api/Department/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            var department = await _hRDbContext.Departments.FindAsync(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return StatusCode(StatusCodes.Status200OK, department);
+        }
+
+        // POST: api/Department
+        [HttpPost]
+        public async Task<ActionResult<Department>> PostDepartment(Department department)
+        {
+            _hRDbContext.Departments.Add(department);
+            await _hRDbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status201Created, CreatedAtAction("GetDepartment", new { id = department.Id }, department));
+        }
+
+        // PUT: api/Department/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDepartment(int id, Department department)
+        {
+            if (id != department.Id)
+            {
+                return BadRequest();
+            }
+
+            _hRDbContext.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                await _hRDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(StatusCodes.Status200OK, department);
+        }
+
+        // DELETE: api/Department/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            var department = await _hRDbContext.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            _hRDbContext.Departments.Remove(department);
+            await _hRDbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return _hRDbContext.Departments.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HRSystemAPI/Controllers/PersonsController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HRSystemAPI.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRSystemAPI.Data;
+
+namespace HRSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly HRDbContext _hRDbContext;
+
+        public PersonsController(HRDbContext hRDbContext)
+        {
+            _hRDbContext = hRDbContext;
+        }
+
+        // GET: api/Persons
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
+        {
+            return await _hRDbContext.Persons.ToListAsync();
+        }
+
+        // GET: api/Persons/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(int id)
+        {
+            var person = await _hRDbContext.Persons.FindAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+
+        // PUT: api/Persons/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePerson(int id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            _hRDbContext.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _hRDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Persons
+        [HttpPost]
+        public async Task<ActionResult<Person>> AddPerson(Person person)
+        {
+            _hRDbContext.Persons.Add(person);
+            await _hRDbContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new { id = person.Id }, person);
+        }
+
+        // DELETE: api/Persons/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            var person = await _hRDbContext.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _hRDbContext.Persons.Remove(person);
+            await _hRDbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _hRDbContext.Persons.Any(e => e.Id == id);
+        }
+    }
+}
